4d0a80e146aa455b6d7a79cc1b1cb169f34989ee
RomanSchaposhnikov <roman.schaposhnikov@gmail.com>
2021-12-12 02:55:49 +0300

    New version

--- todoist_bot/run_bot.py
+++ todoist_bot/run_bot.py
@@ -7,6 +7,5 @@ from config import TODOIST_TOKEN
 api = TodoistAPI(TODOIST_TOKEN)
 
 if __name__ == "__main__":
-    print(TODOIST_TOKEN)
     run_polling(api)
-
+    
--- todoist_bot/servises.py
+++ todoist_bot/servises.py
@@ -2,35 +2,49 @@ from todoist import TodoistAPI
 from loguru import logger as log
 
 
-def improve_structure(api: TodoistAPI):
-    static_projects = ["Team Inbox", "Inbox", "ToDo", "Someday", "Notions"]
-
+def change_color_if_first_step_assigned(api: TodoistAPI) -> None:
     api.sync()
 
-    for project in api.state["projects"]:
+    list_of_projects = [
+        p for p in api.state["projects"]
+        if (
+            (not isinstance(p["id"], str)) and (p["parent_id"] == 2279765501)
+            and (p["color"] != 33)
+        )
+    ]
 
-        if project["name"] == "ToDo":
-            for item in api.projects.get_data(project["id"])["items"]:
-                api.items.get_by_id(item["id"]).update(labels=[2159063863])
+    for p in list_of_projects:
+
+        project_data = api.projects.get_data(p["id"])
+        first_step = project_data["sections"][0]
+        list_of_items = project_data["items"] 
+        
+        for i in list_of_items:
+            if i["section_id"] == first_step["id"]:
+                p.update(color=36)
+                #p.reorder(child_order=-1)
+                break
+        else:
+            p.update(color=46)
+            p.reorder(child_order=1)
+
+    api.commit()
 
 
-        if not (project["name"] in static_projects) and isinstance(project["id"], int):
+def shape_new_projects(api: TodoistAPI) -> None:
+    api.sync()
 
-            if project["color"] == 47:
-                api.templates.import_into_project(project["id"], "project.csv")
+    list_of_projects = [
+        p for p in api.state["projects"]
+        if (
+            (not isinstance(p["id"], str)) and (p["parent_id"] == 2279765501)
+            and (p["color"] == 33)
+        )
+    ]
 
-            content = api.projects.get_data(project["id"])
-
-            first_step = content["sections"][0]
-            items = content["items"]
-
-            for item in items:
-                if item["section_id"] == first_step["id"]:
-                    project.update(color=36)
-                    break
-                
-                project.update(color=46)
-                
+    for p in list_of_projects:
+        api.templates.import_into_project(p["id"], "todoist_bot/template_project.csv")
+        p.update(color=46)
 
     api.commit()
 
@@ -39,5 +53,6 @@ def run_polling(api: TodoistAPI):
     log.info("Bot started in polling mod!")
     
     while True:
-        improve_structure(api)
-   
+        shape_new_projects(api)
+        change_color_if_first_step_assigned(api)
+
